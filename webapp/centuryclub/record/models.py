48212b3dc7efcf02c2a277e2d2165e8ceb159575
Sean Fitzgerald <seanf0617@gmail.com>
2022-05-24 08:25:37 -0400

    Started adding the record functionality

--- webapp/centuryclub/record/models.py
+++ webapp/centuryclub/record/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class RecordActivity(models.Model):
+    pass
+    # add additional fields in here
+    username = models.CharField(null=True, blank=False, max_length=150)
+    distance = models.IntegerField(blank=True, null=True)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # def __str__(self):
+    #     return self.name
