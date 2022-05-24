76c547a54462d8c3d9fa232c653573342b90c561
Sean Fitzgerald <seanf0617@gmail.com>
2022-05-24 18:18:15 -0400

    Added Exercise type to DB

--- webapp/centuryclub/record/admin.py
+++ webapp/centuryclub/record/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import RecordActivity
+from .models import ExerciseType, RecordActivity, RecordMonthTarget
+
 
 # Register your models here.
 admin.site.register(RecordActivity)
+admin.site.register(RecordMonthTarget)
+admin.site.register(ExerciseType)
+
--- webapp/centuryclub/record/models.py
+++ webapp/centuryclub/record/models.py
@@ -1,14 +1,25 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class RecordActivity(models.Model):
     pass
     # add additional fields in here
     username = models.CharField(null=True, blank=False, max_length=150)
-    distance = models.IntegerField(blank=True, null=True)
+    exercisetype = models.CharField(null=True, blank=False, max_length=50)
+    distance = models.DecimalField(max_digits=5, decimal_places=2)
     date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # def __str__(self):
     #     return self.name
+
+class RecordMonthTarget(models.Model):
+    username = models.CharField(null=True, blank=False, max_length=150)
+    targetdistance = models.IntegerField(blank=True, null=True)
+    targetdate = models.DateField(default=datetime.date.today)
+
+
+class ExerciseType(models.Model):
+    exercisetype = models.CharField(null=True, blank=False, max_length=50)
