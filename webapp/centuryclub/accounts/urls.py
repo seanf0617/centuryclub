3a186497b2b07a8fffa02933060aa05c97110f9b
Sean Fitzgerald <seanf0617@gmail.com>
2022-06-02 09:11:39 -0400

    Add edit user but need to fill in data to edit and then save

--- webapp/centuryclub/accounts/urls.py
+++ webapp/centuryclub/accounts/urls.py
@@ -1,11 +1,12 @@
 from . import views
 from django.urls import path, include
-from .views import SignUpView
+from .views import SignUpView,EditView
 
 
 urlpatterns = [
    path('', views.index, name='index'),
    path("accounts/", include("django.contrib.auth.urls")),
    path("accounts/signup/", SignUpView.as_view(), name="signup"),
+   path("accounts/useredit/", EditView.as_view(), name="useredit"),
 
 ]
--- webapp/centuryclub/accounts/views.py
+++ webapp/centuryclub/accounts/views.py
@@ -6,7 +6,7 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm,CustomUserChangeForm
 
 
 # Create your views here.
@@ -21,11 +21,17 @@ class SignUpView(CreateView):
     template_name = "registration/signup.html"
 
 
+class EditView(CreateView):
+    form_class = CustomUserChangeForm
+    
+    success_url = reverse_lazy("useredit")
+    template_name = "registration/useredit.html"
+
+
 def Login(request):
     if request.method == 'POST':
   
         # AuthenticationForm_can_also_be_used__
-  
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username = username, password = password)
@@ -34,6 +40,6 @@ def Login(request):
             messages.success(request, f' Welcome {username} !!')
             return redirect('home')
         else:
-            messages.info(request, f'account done not exit plz sign in')
+            messages.info(request, f'account does not exit plz sign in')
     form = AuthenticationForm()
     return render(request, 'user/login.html', {'form':form, 'title':'log in'})
